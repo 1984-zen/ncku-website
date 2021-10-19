5296e6bb869214de0ec6dd19fd5f03767a58f281
Zen <1984.apple@gmail.com>
2021-10-19 20:15:31 +0800

    feat:add header menu RWD

--- common.js
+++ common.js
@@ -1,3 +1,11 @@
+$(window).scroll(function() {
+    if ($(this).scrollTop() > 60)  {         /* 要滑動到選單的距離 */
+        $('header').addClass('navFixed');    /* 幫選單加上固定效果 */
+    } else {
+        $('header').removeClass('navFixed'); /* 移除選單固定效果 */
+    }
+});
+
 $('button').click(function(){ //只有用HTML的標籤和id能夠分辨出使用者是按了哪個按鈕，為了減少重複一樣的的code，所以決定用button標籤去做，在這個網站，只有播放影片才會使用button標籤
     var videoName = $(this).attr('data-videoName'); // 抓出摸到這個 button 的 data-videoName 值
     var videoPath = `./videos/${videoName}.mp4`;
@@ -11,3 +19,8 @@ $('button').click(function(){ //只有用HTML的標籤和id能夠分辨出使用
 $('.video_closebtn').click(function(){
     $("#video_light_box").fadeOut(400); // 讓用來播放 video 的父層css改為 display: none
 });
+
+function searchFunction() {
+    let myText = document.getElementById("myText").value;
+    location.assign(`https://www.ncbi.nlm.nih.gov/medgen/?term=${myText}`)
+}
