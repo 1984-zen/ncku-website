90dcd51c0baf8c20382b4ccbf31e485a206519eb
Zen <1984.apple@gmail.com>
2021-10-20 20:18:13 +0800

    feat:index.html add phone RWD

--- common.js
+++ common.js
@@ -6,6 +6,29 @@ $(window).scroll(function() {
     }
 });
 
+// ===============================偵測螢幕寬度 start
+// 瀏覽器在 refresh 的時候做一次性判斷(refresh 當下只做一次)
+$(document).ready(function() {
+    if ($(this).width() > 768)  {
+        $('.big_banner_container_mobile').addClass('big_banner_container_desktop');
+        $('.big_banner_container_desktop').removeClass('big_banner_container_mobile');
+    } else {
+        $('.big_banner_container_desktop').addClass('big_banner_container_mobile');
+        $('.big_banner_container_mobile').removeClass('big_banner_container_desktop');
+    }
+})
+// 當瀏覽器在觸發寬度調整的時候執行(即時)
+$(window).resize(function() {
+    if ($(this).width() > 768)  {
+        $('.big_banner_container_mobile').addClass('big_banner_container_desktop');
+        $('.big_banner_container_desktop').removeClass('big_banner_container_mobile');
+    } else {
+        $('.big_banner_container_desktop').addClass('big_banner_container_mobile');
+        $('.big_banner_container_mobile').removeClass('big_banner_container_desktop');
+    }
+});
+// ===============================偵測螢幕寬度 end
+
 $('button').click(function(){ //只有用HTML的標籤和id能夠分辨出使用者是按了哪個按鈕，為了減少重複一樣的的code，所以決定用button標籤去做，在這個網站，只有播放影片才會使用button標籤
     var videoName = $(this).attr('data-videoName'); // 抓出摸到這個 button 的 data-videoName 值
     var videoPath = `./videos/${videoName}.mp4`;
